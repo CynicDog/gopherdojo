da383454a83eb1edbffe5f87c650b521dfa8e263
eunsang <thecynicdog0328@gmail.com>
2025-10-07 11:14:51 +0900

    chore:

---
+++ go-by-example/ch02/01_groundwork/go.mod
@@ -0,0 +1,3 @@
+module github.com/cynicdog/gopherdojo/go-by-example/01_groundwork
+
+go 1.25.0
---
+++ go-by-example/ch02/01_groundwork/url/cmd/url.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/cynicdog/gopherdojo/go-by-example/01_groundwork/url"
+)
+
+func main() {
+	uri, err := url.Parse("https://github.com/inancgumus")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Scheme:", uri.Scheme)
+	fmt.Println("Host  :", uri.Host)
+	fmt.Println("Path  :", uri.Path)
+	fmt.Println(uri)
+}
